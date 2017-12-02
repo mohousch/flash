b10bf82178e601d1442ebf459e22fe46d1598fc8
max_10 <max_10@gmx.de>
2017-12-02 15:22:49 +0100

    update gitversion

--- common/gitversion.sh
+++ common/gitversion.sh
@@ -3,22 +3,20 @@
 CURDIR=$1
 DATETIME=_`date +%d.%m.%Y-%H.%M`
 
-if [ -d $CURDIR/../../source/libstb-hal-cst-next ]; then
-	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-cst-next && git log | grep "^commit" | wc -l`-github
-elif [ -d $CURDIR/../../source/libstb-hal-cst-next-max ]; then
-	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-cst-next-max && git log | grep "^commit" | wc -l`-max
-elif [ -d $CURDIR/../../source/libstb-hal-cst-next-tangos ]; then
-	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-cst-next-tangos && git log | grep "^commit" | wc -l`-tangos
+if [ -d $CURDIR/../../source/libstb-hal-ddt ]; then
+	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-ddt git log | grep "^commit" | wc -l`-ddt
+elif [ -d $CURDIR/../../source/libstb-hal-max ]; then
+	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-max && git log | grep "^commit" | wc -l`-max
+elif [ -d $CURDIR/../../source/libstb-hal-tangos ]; then
+	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-tangos && git log | grep "^commit" | wc -l`-tangos
 else
 	HAL_REV=_HAL-revXXX
 fi
 
-if [ -d $CURDIR/../../source/neutrino-mp-cst-next ]; then
-	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-cst-next && git log | grep "^commit" | wc -l`-github
-elif [ -d $CURDIR/../../source/neutrino-mp-cst-next-ni ]; then
-	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-cst-next-ni && git log | grep "^commit" | wc -l`-github
-elif [ -d $CURDIR/../../source/neutrino-mp-cst-next-max ]; then
-	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-cst-next-max && git log | grep "^commit" | wc -l`-max
+if [ -d $CURDIR/../../source/neutrino-mp-ddt ]; then
+	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-ddt git log | grep "^commit" | wc -l`-ddt
+elif [ -d $CURDIR/../../source/neutrino-mp-max ]; then
+	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-max && git log | grep "^commit" | wc -l`-max
 elif [ -d $CURDIR/../../source/neutrino-mp-tangos ]; then
 	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-tangos && git log | grep "^commit" | wc -l`-tangos
 else
