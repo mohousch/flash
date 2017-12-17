616ee38618d767c6c35264fa581c770727dd3020
Frankenstone <dampf_acc@yahoo.com>
2017-12-17 21:42:55 +0100

    fix gitversion

--- common/gitversion.sh
+++ common/gitversion.sh
@@ -4,7 +4,7 @@ CURDIR=$1
 DATETIME=_`date +%d.%m.%Y-%H.%M`
 
 if [ -d $CURDIR/../../source/libstb-hal-ddt ]; then
-	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-ddt git log | grep "^commit" | wc -l`-ddt
+	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-ddt && git log | grep "^commit" | wc -l`-ddt
 elif [ -d $CURDIR/../../source/libstb-hal-max ]; then
 	HAL_REV=_HAL-rev`cd $CURDIR/../../source/libstb-hal-max && git log | grep "^commit" | wc -l`-max
 elif [ -d $CURDIR/../../source/libstb-hal-tangos ]; then
@@ -14,7 +14,7 @@ else
 fi
 
 if [ -d $CURDIR/../../source/neutrino-mp-ddt ]; then
-	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-ddt git log | grep "^commit" | wc -l`-ddt
+	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-ddt && git log | grep "^commit" | wc -l`-ddt
 elif [ -d $CURDIR/../../source/neutrino-mp-max ]; then
 	NMP_REV=_NMP-rev`cd $CURDIR/../../source/neutrino-mp-max && git log | grep "^commit" | wc -l`-max
 elif [ -d $CURDIR/../../source/neutrino-mp-tangos ]; then
