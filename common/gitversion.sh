f12125121a57296729db71c8fa8555cc1208c838
BPanther <bpanther_ts@hotmail.com>
2020-05-17 14:12:25 +0200

    fix last commit

--- common/gitversion.sh
+++ common/gitversion.sh
@@ -6,7 +6,7 @@ DATETIME=_`date +%d.%m.%Y-%H.%M`
 if [ -d $CURDIR/../../build_source/libstb-hal-ddt ]; then
 	HAL_REV=_HAL-rev`cd $CURDIR/../../build_source/libstb-hal-ddt && git log | grep "^commit" | wc -l`-ddt
 elif [ -d $CURDIR/../../build_source/ni-libstb-hal ]; then
-	HAL_REV=_HAL-rev`cd $CURDIR/../../build_source/ni-libstb-hal && git log | grep "^commit" | wc -l`-nu
+	HAL_REV=_HAL-rev`cd $CURDIR/../../build_source/ni-libstb-hal && git log | grep "^commit" | wc -l`-ni
 elif [ -d $CURDIR/../../build_source/libstb-hal-tangos ]; then
 	HAL_REV=_HAL-rev`cd $CURDIR/../../build_source/libstb-hal-tangos && git log | grep "^commit" | wc -l`-tangos
 else
