77e8998c3495b4d9a99b688ec79f58a6d9d8d429
BPanther <bpanther_ts@hotmail.com>
2020-05-17 14:07:47 +0200

    fix revXXXX

--- common/gitversion.sh
+++ common/gitversion.sh
@@ -5,24 +5,20 @@ DATETIME=_`date +%d.%m.%Y-%H.%M`
 
 if [ -d $CURDIR/../../build_source/libstb-hal-ddt ]; then
 	HAL_REV=_HAL-rev`cd $CURDIR/../../build_source/libstb-hal-ddt && git log | grep "^commit" | wc -l`-ddt
-elif [ -d $CURDIR/../../build_source/libstb-hal-max ]; then
-	HAL_REV=_HAL-rev`cd $CURDIR/../../build_source/libstb-hal-max && git log | grep "^commit" | wc -l`-max
+elif [ -d $CURDIR/../../build_source/ni-libstb-hal ]; then
+	HAL_REV=_HAL-rev`cd $CURDIR/../../build_source/ni-libstb-hal && git log | grep "^commit" | wc -l`-nu
 elif [ -d $CURDIR/../../build_source/libstb-hal-tangos ]; then
 	HAL_REV=_HAL-rev`cd $CURDIR/../../build_source/libstb-hal-tangos && git log | grep "^commit" | wc -l`-tangos
-elif [ -d $CURDIR/../../build_source/neutrino-hd2 ]; then
-	HAL_REV=
 else
 	HAL_REV=_HAL-revXXX
 fi
 
-if [ -d $CURDIR/../../build_source/neutrino-mp-ddt ]; then
-	NMP_REV=_NMP-rev`cd $CURDIR/../../build_source/neutrino-mp-ddt && git log | grep "^commit" | wc -l`-ddt
-elif [ -d $CURDIR/../../build_source/neutrino-mp-max ]; then
-	NMP_REV=_NMP-rev`cd $CURDIR/../../build_source/neutrino-mp-max && git log | grep "^commit" | wc -l`-max
-elif [ -d $CURDIR/../../build_source/neutrino-mp-tangos ]; then
-	NMP_REV=_NMP-rev`cd $CURDIR/../../build_source/neutrino-mp-tangos && git log | grep "^commit" | wc -l`-tangos
-elif [ -d $CURDIR/../../build_source/neutrino-hd2 ]; then
-	NMP_REV=_NHD2-rev`cd $CURDIR/../../build_source/neutrino-hd2 && git log | grep "^commit" | wc -l`
+if [ -d $CURDIR/../../build_source/neutrino-ddt ]; then
+	NMP_REV=_NMP-rev`cd $CURDIR/../../build_source/neutrino-ddt && git log | grep "^commit" | wc -l`-ddt
+elif [ -d $CURDIR/../../build_source/ni-neutrino ]; then
+	NMP_REV=_NMP-rev`cd $CURDIR/../../build_source/ni-neutrino && git log | grep "^commit" | wc -l`-ni
+elif [ -d $CURDIR/../../build_source/neutrino-tangos ]; then
+	NMP_REV=_NMP-rev`cd $CURDIR/../../build_source/neutrino-tangos && git log | grep "^commit" | wc -l`-tangos
 else
 	NMP_REV=_NMP-revXXX
 fi
